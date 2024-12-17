d1839c2e248a0c3d95435df6256fe576dd3573d0
satthurikranthi <satthurikranthi@gmail.com>
2024-12-17 16:36:43 +0530

    Add files via upload

---
+++ version controller/branches/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div className="App">
+    <h1>fidaa</h1>
+    <h1>fidaa 2</h1>
+    <img src="./saipallavi/saipallavi.webp"></img>
+
+    </div>
+  );
+}
+
+export default App;
